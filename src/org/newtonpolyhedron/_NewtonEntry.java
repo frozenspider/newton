7946d8fbb0c7f136f26117c0a8d3012c582d37d2
Alex Abd <Alexander.Abdugafarov@nitka.com>
2014-12-24 03:26:48 +0500

    Reformatted all sources, changed line endings to \n

--- src/org/newtonpolyhedron/_NewtonEntry.java
+++ src/org/newtonpolyhedron/_NewtonEntry.java
@@ -1,8 +1,8 @@
 package org.newtonpolyhedron;
 
 public class _NewtonEntry {
-	
-	public static void main(String[] args) {
-		_NewtonEntryScala.main(args);
-	}
+
+    public static void main(String[] args) {
+        _NewtonEntryScala.main(args);
+    }
 }
--- src/org/newtonpolyhedron/ex/UnknownModeException.java
+++ src/org/newtonpolyhedron/ex/UnknownModeException.java
@@ -3,10 +3,10 @@ package org.newtonpolyhedron.ex;
 import org.newtonpolyhedron.WorkingMode;
 
 public class UnknownModeException extends Exception {
-	
-	private static final long	serialVersionUID	= 7864040664662041730L;
-	
-	public UnknownModeException(final WorkingMode mode) {
-		super(String.valueOf(mode));
-	}
+
+    private static final long serialVersionUID = 7864040664662041730L;
+
+    public UnknownModeException(final WorkingMode mode) {
+        super(String.valueOf(mode));
+    }
 }
