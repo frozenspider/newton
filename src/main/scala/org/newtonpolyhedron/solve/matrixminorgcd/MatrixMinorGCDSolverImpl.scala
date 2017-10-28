aa0f6822b045f517569b635c2aade3c7696b40ce
Alex Abdugafarov <fswork90@gmail.com>
2017-10-28 16:38:43 +0500

    Fixed mistake done during refactoring

--- src/main/scala/org/newtonpolyhedron/solve/matrixminorgcd/MatrixMinorGCDSolverImpl.scala
+++ src/main/scala/org/newtonpolyhedron/solve/matrixminorgcd/MatrixMinorGCDSolverImpl.scala
@@ -8,8 +8,10 @@ class MatrixMinorGCDSolverImpl[N <: MPNumber](implicit mp: MathProcessor[N])
     require(matrix.isSquare, "Matrix must be square (although last row is zeros)")
     val dets = for (skipCol <- 0 until matrix.colCount) yield {
       val minor = matrix.minorMatrix(matrix.rowCount - 1, skipCol).det
-      assert(minor.isRational, "Non-integer minor")
-      minor.toRational.numerator
+      assert(minor.isRational, "Irrational minor")
+      val minorRat = minor.toRational
+      assert(minorRat.isWhole, "Non-integer minor")
+      minorRat.numerator
     }
     val gcd = dets.reduceLeft(_ gcd _)
     (gcd.toBigInt, dets map (_.toBigInt))
