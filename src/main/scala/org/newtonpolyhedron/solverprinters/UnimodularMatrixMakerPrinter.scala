a9336b75deef4ad4e9efc7bf725b5b0d1987d790
Alex Abdugafarov <fswork90@gmail.com>
2017-10-28 16:34:25 +0500

    Workaround for https://github.com/scala/bug/issues/10573

--- src/main/scala/org/newtonpolyhedron/solverprinters/UnimodularMatrixMakerPrinter.scala
+++ src/main/scala/org/newtonpolyhedron/solverprinters/UnimodularMatrixMakerPrinter.scala
@@ -20,16 +20,17 @@ class UnimodularMatrixMakerPrinter[N <: MPNumber](
   ) = {
     val alpha = solver.unimodularFrom(baseMatrix)
     val alphaInv = alpha.inverse
+    // Using .toString as a workaround for https://github.com/scala/bug/issues/10573
     output.println(title("""Unimodular "Alpha" matrix"""))
     val text1 = new StringBuilder()
     text1 ++= subheader("Base matrix:") + "\n"
-    text1 ++= baseMatrix + "\n"
+    text1 ++= baseMatrix.toString + "\n"
     val text2 = new StringBuilder()
     text2 ++= subheader("Alpha-matrix:") + "\n"
-    text2 ++= alpha + "\n"
+    text2 ++= alpha.toString + "\n"
     val text3 = new StringBuilder()
     text3 ++= subheader("Inverse alpha-matrix:") + "\n"
-    text3 ++= alphaInv + "\n"
+    text3 ++= alphaInv.toString + "\n"
     output.println(StringUtils.appendToRight(5, text1, text2, text3))
   }
 }
